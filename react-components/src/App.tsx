d9e945fe694eb7f748743869e9fcf92e0eb52bd0
PaulRomanov <ramosha@mail.ru>
2021-07-29 15:38:53 +0300

    feat: add new code

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -1,12 +1,14 @@
 import './styles.scss';
-import IMAGE from './assets/react-logo.png';
+// import IMAGE from './assets/react-logo.png';
+import SearchEnginePage from './components/searchEnginePage/SearchEnginePage';
 
 export const App = () => {
   return (
-    <>
-      <h1>React TypeScript Webpack Started Template</h1>
-      <img src={IMAGE} alt="react-logo" width="300" height="200" />
-    </>
+    <div>
+      {/* <h1>React TypeScript Webpack Started Template</h1>
+      <img src={IMAGE} alt="react-logo" width="300" height="200" /> */}
+      <SearchEnginePage />
+    </div>
   );
 };
 
