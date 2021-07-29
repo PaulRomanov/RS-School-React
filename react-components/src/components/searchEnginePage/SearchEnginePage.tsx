7c9533936c3032150af9afa9a9ed113001765bff
PaulRomanov <ramosha@mail.ru>
2021-07-29 15:38:23 +0300

    feat: add component search page

---
+++ react-components/src/components/searchEnginePage/SearchEnginePage.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './searchEnginePage.scss';
+
+const SearchEnginePage = () => {
+  return (
+    <div className="serch-page">
+      <form className="form-wrapper">
+        <input id="search" placeholder="Site search..."  type="text" />
+          <input value="Поиск" id="submit" type="submit" />
+        </form>
+    </div>
+    
+    
+
+        )
+}
+
+export default SearchEnginePage;
