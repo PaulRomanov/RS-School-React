9341620d7e30b49119ebdfd93e3919cbc7b095ce
PaulRomanov <ramosha@mail.ru>
2021-08-07 13:32:36 +0300

    feat: add component radio btn

--- react-api/src/App.tsx
+++ react-api/src/App.tsx
@@ -1,10 +1,13 @@
 import './styles.scss';
-import SearchLine from './components/searchLine/SearchLine';
+import {SearchLine} from './components/searchLine/SearchLine';
+import { RadioBtn } from './components/radioBtn/radioBtn';
+
 
 export const App = () => {
   return (
     <>
       <SearchLine />
+      <RadioBtn />
     </>
   );
 };
