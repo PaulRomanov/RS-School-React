f67cdf09be9bed1fcdb5c9e0930274ac9733f4ad
PaulRomanov <ramosha@mail.ru>
2021-08-11 15:05:26 +0300

    feat: add rule

--- react-api/.eslintrc.js
+++ react-api/.eslintrc.js
@@ -24,6 +24,12 @@ module.exports = {
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        'endOfLine': 'auto',
+      }
+    ]
   },
   settings: {
     react: {
