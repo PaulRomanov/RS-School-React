a7f2a6c1cce29d0fe0d7f09c17eef4f11478bade
PaulRomanov <ramosha@mail.ru>
2021-08-11 13:38:49 +0300

    feat: delete files

---
+++ react-api/src/components/card/Card.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainPage from '../../pages/MainPage';
+
+const Card = ({ author }) => {
+  return (
+    <div>
+      <p>{state.author}</p>
+    </div>
+  );
+};
+
+export default Card;
