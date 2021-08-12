7ebe1e0255b23ddabcb2e39b8fb67d0931fdfee7
PaulRomanov <ramosha@mail.ru>
2021-08-12 13:48:22 +0300

    feat: add functional on button

--- react-api/src/components/searchLine/SearchLine.tsx
+++ react-api/src/components/searchLine/SearchLine.tsx
@@ -2,7 +2,7 @@ import { AxiosResponse } from 'axios';
 import React, { ChangeEvent, FC, useState } from 'react';
 import axiosInstance from '../../services/api';
 import './searchLine.scss';
-import { Props } from '../../type';
+import { GET200_Articles, Props } from '../../type';
 // import Card from '../card/Card';
 
 const API_KEY = '6acc09f802644746b9fafbaeda30a3d6';
@@ -16,19 +16,22 @@ const SearchLine: FC<Props> = ({ setState }) => {
     e.preventDefault();
     setIsLoading(true);
     try {
-      const response: AxiosResponse<any> = await axiosInstance.get(
-        // `api/json/v1/1/filter.php?c=${searchValue}`,
+      const response: AxiosResponse<GET200_Articles> = await axiosInstance.get(
+        // `api/json/v1/1/filter.php?i=${searchValue}`,
         `v2/everything?q=${searchValue}&apiKey=${API_KEY}`,
       );
+
       // setState(response.data.drinks);
       setState(response.data.articles);
+      // console.log(response.data.drinks);
 
       // return response;
     } catch (err: any) {
-      // console.error(e);
+      console.error(e);
     } finally {
       setIsLoading(false);
     }
+    return Response;
   };
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -48,7 +51,9 @@ const SearchLine: FC<Props> = ({ setState }) => {
               onChange={handleChange}
               className="input"
             />
-            <input value="Поиск" id="submit" type="submit" disabled={isLoading} />
+            <button id="submit" type="submit" disabled={isLoading}>
+              {isLoading ? 'Loding...' : 'Search'}
+            </button>
           </label>
         </form>
       </div>
