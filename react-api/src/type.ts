4d9b88d043da8d5fbfd5455da80c7c907e544648
PaulRomanov <ramosha@mail.ru>
2021-08-12 13:48:00 +0300

    fix: types

--- react-api/src/type.ts
+++ react-api/src/type.ts
@@ -1,10 +1,10 @@
-export type CardType = {
+export interface CardType {
   author: string;
   content: string;
   title: string;
   urlToImage: string;
   // strDrink: string;
-};
+}
 
 export type CardProps = {
   itemElement: CardType;
@@ -13,3 +13,8 @@ export type CardProps = {
 export type Props = {
   setState: React.Dispatch<React.SetStateAction<CardType[]>>;
 };
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export interface GET200_Articles {
+  articles: CardType[];
+}
