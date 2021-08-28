76311a4089a4ea7a670a6bc5ec263f0ec92c5e75
PaulRomanov <ramosha@mail.ru>
2021-08-28 14:28:45 +0300

    feat: add page

--- react-router/src/pages/NotFound.tsx
+++ react-router/src/pages/NotFound.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './notFound.scss';
 
 const NotFound = () => {
-  return <div>The resource can not be found</div>;
+  return (
+    <div>
+      <nav>
+        <NavLink exact activeClassName="activ-link" className="shine-button btn-404" to="/">
+          Home
+        </NavLink>
+      </nav>
+      <img className="img-404" src="assets/404.png" alt="404" />
+    </div>
+  );
 };
 
 export default NotFound;
